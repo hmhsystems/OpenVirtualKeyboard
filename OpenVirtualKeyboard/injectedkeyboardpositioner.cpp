7bd904d67d85d707e4f0e479fc99889cdf077a31
Augusto Grabe <augustograbe@gmail.com>
2025-10-03 14:14:47 -0300

    fix: position based on dpiScale parameter

--- OpenVirtualKeyboard/injectedkeyboardpositioner.cpp
+++ OpenVirtualKeyboard/injectedkeyboardpositioner.cpp
@@ -5,64 +5,66 @@
  */
 
 #include "injectedkeyboardpositioner.h"
-#include <chrono>
 #include <QGuiApplication>
 #include <QQuickItem>
 #include <QQuickWindow>
+#include <QScreen>
 #include <QTimer>
+#include <chrono>
 
-InjectedKeyboardPositioner::InjectedKeyboardPositioner( bool noContentScroll )
-    : _scrollContentItem( !noContentScroll )
-{ }
+InjectedKeyboardPositioner::InjectedKeyboardPositioner(bool noContentScroll)
+    : _scrollContentItem(!noContentScroll)
+{
+}
 
 InjectedKeyboardPositioner::~InjectedKeyboardPositioner() = default;
 
-void InjectedKeyboardPositioner::setKeyboardObject( QObject* keyboardObject )
+void InjectedKeyboardPositioner::setKeyboardObject(QObject* keyboardObject)
 {
-    _keyboard = qobject_cast<QQuickItem*>( keyboardObject );
+    _keyboard = qobject_cast<QQuickItem*>(keyboardObject);
     if (!_keyboard)
         return;
 
-    init( _keyboard );
+    init(_keyboard);
 
-    connect( _keyboard,
-             &QQuickItem::heightChanged,
-             this,
-             &InjectedKeyboardPositioner::onHeightChanged );
+    connect(_keyboard,
+        &QQuickItem::heightChanged,
+        this,
+        &InjectedKeyboardPositioner::onHeightChanged);
 
-    updateFocusItem( _focusItem );
+    updateFocusItem(_focusItem);
 
     if (_contentItem)
-        _keyboard->setY( _contentItem->height() ); // inicia fora da tela (abaixo)
+        _keyboard->setY(_contentItem->height()); // inicia fora da tela (abaixo)
 
-    connect( qGuiApp,
-             &QGuiApplication::applicationStateChanged,
-             this,
-             &InjectedKeyboardPositioner::onApplicationStateChanged );
-    onApplicationStateChanged( qGuiApp->applicationState() );
+    connect(qGuiApp,
+        &QGuiApplication::applicationStateChanged,
+        this,
+        &InjectedKeyboardPositioner::onApplicationStateChanged);
+    onApplicationStateChanged(qGuiApp->applicationState());
 }
 
-void InjectedKeyboardPositioner::enableAnimation( bool enabled )
+void InjectedKeyboardPositioner::enableAnimation(bool enabled)
 {
     if (enabled) {
-        _animation.reset( new QPropertyAnimation );
-        _animation->setPropertyName( "y" );
-        _animation->setEasingCurve( QEasingCurve( QEasingCurve::OutCubic ));
+        _animation.reset(new QPropertyAnimation);
+        _animation->setPropertyName("y");
+        _animation->setEasingCurve(QEasingCurve(QEasingCurve::OutCubic));
 
-        connect( _animation.get(),
-                 &QAbstractAnimation::stateChanged,
-                 this,
-                 &AbstractPositioner::animatingChanged );
-        connect( _animation.get(),
-                 &QAbstractAnimation::finished,
-                 this,
-                 &InjectedKeyboardPositioner::onAnimationFinished );
+        connect(_animation.get(),
+            &QAbstractAnimation::stateChanged,
+            this,
+            &AbstractPositioner::animatingChanged);
+        connect(_animation.get(),
+            &QAbstractAnimation::finished,
+            this,
+            &InjectedKeyboardPositioner::onAnimationFinished);
     } else {
         _animation.reset();
     }
 }
 
-void InjectedKeyboardPositioner::updateFocusItem( QQuickItem* focusItem )
+void InjectedKeyboardPositioner::updateFocusItem(QQuickItem* focusItem)
 {
     if (_focusItem != focusItem)
         _focusItemChanged = true;
@@ -71,22 +73,21 @@ void InjectedKeyboardPositioner::updateFocusItem( QQuickItem* focusItem )
     if (!_focusItem || !_keyboard)
         return;
 
-    auto window      = _focusItem->window();
+    auto window = _focusItem->window();
     auto contentItem = window ? window->contentItem() : nullptr;
 
     if (contentItem == _contentItem)
         return;
 
     if (_contentItem)
-        _contentItem->disconnect( this );
+        _contentItem->disconnect(this);
 
     _contentItem = contentItem;
-    _keyboard->setParentItem( _contentItem );
-    connect( _contentItem,
-             &QQuickItem::heightChanged,
-             this,
-             &InjectedKeyboardPositioner::onHeightChanged );
-
+    _keyboard->setParentItem(_contentItem);
+    connect(_contentItem,
+        &QQuickItem::heightChanged,
+        this,
+        &InjectedKeyboardPositioner::onHeightChanged);
 }
 
 void InjectedKeyboardPositioner::show()
@@ -95,38 +96,38 @@ void InjectedKeyboardPositioner::show()
     // called setFocusObject() and showInputPanel() in wrong order (for our purposes)
     // and this way we walked around some UI imperfect behaviour.
 
-    std::chrono::milliseconds milliseconds( _animation ? _appStateReactivated ? 100 : 0 : 0 );
+    std::chrono::milliseconds milliseconds(_animation ? _appStateReactivated ? 100 : 0 : 0);
 
-    QTimer::singleShot( milliseconds, this, [this] {
+    QTimer::singleShot(milliseconds, this, [this] {
         if (!_keyboard || !_contentItem) {
             _shown = false;
             return;
         }
 
         bool alreadyShown = _shown;
-        _shown            = true;
+        _shown = true;
 
         if (alreadyShown) {
             if (_focusItemChanged) {
-                updateContentItemPosition( true );
+                updateContentItemPosition(true);
                 _focusItemChanged = false;
             }
             return;
         }
 
         _focusItemChanged = false;
-        updateContentItemPosition( true );
+        updateContentItemPosition(true);
 
         if (_animation) {
-            _animation->setTargetObject( nullptr );
-            _animation->setStartValue( _contentItem->height() + _offset );
-            _animation->setEndValue( visibleKeyboardTopY() + _offset );
-            _animation->setTargetObject( _keyboard );
+            _animation->setTargetObject(nullptr);
+            _animation->setStartValue(_contentItem->height() + _offset);
+            _animation->setEndValue(visibleKeyboardTopY() + _offset);
+            _animation->setTargetObject(_keyboard);
             _animation->start();
         } else {
-            _keyboard->setY( visibleKeyboardTopY() + _offset );
+            _keyboard->setY(visibleKeyboardTopY() + _offset);
         }
-    } );
+    });
 }
 
 void InjectedKeyboardPositioner::hide()
@@ -137,14 +138,14 @@ void InjectedKeyboardPositioner::hide()
         return;
 
     if (_animation) {
-        _animation->setTargetObject( nullptr );
-        _animation->setStartValue( _keyboard->y() );
-        _animation->setEndValue( _contentItem->height() + _offset ); // anima para fora da tela (abaixo)
-        _animation->setTargetObject( _keyboard );
+        _animation->setTargetObject(nullptr);
+        _animation->setStartValue(_keyboard->y());
+        _animation->setEndValue(_contentItem->height() + _offset); // anima para fora da tela (abaixo)
+        _animation->setTargetObject(_keyboard);
         _animation->start();
     } else {
-        _contentItem->setY( 0 );
-        _keyboard->setY( _contentItem->height() + _offset );
+        _contentItem->setY(0);
+        _keyboard->setY(_contentItem->height() + _offset);
     }
 }
 
@@ -153,24 +154,24 @@ bool InjectedKeyboardPositioner::isAnimating() const
     return _animation ? _animation->state() == QAbstractAnimation::Running : false;
 }
 
-void InjectedKeyboardPositioner::updateContentItemPosition( bool updateKeyboardPosition )
+void InjectedKeyboardPositioner::updateContentItemPosition(bool updateKeyboardPosition)
 {
     if (!_keyboard || !_contentItem || !_focusItem)
         return;
 
     if (!_shown)
-        _keyboard->setVisible( false );
+        _keyboard->setVisible(false);
 
-    auto focusItemBottom = _contentItem->mapFromItem( _focusItem, QPointF( 0, 0 )).y()
+    auto focusItemBottom = _contentItem->mapFromItem(_focusItem, QPointF(0, 0)).y()
         + _focusItem->height() + 5; // count with 5px spacing
     auto keyboardTop = visibleKeyboardTopY();
     _offset = _scrollContentItem
-                 ? (focusItemBottom > keyboardTop ? (focusItemBottom - keyboardTop) : 0)
-                 : 0;
-    _contentItem->setY( -_offset );
+        ? (focusItemBottom > keyboardTop ? (focusItemBottom - keyboardTop) : 0)
+        : 0;
+    _contentItem->setY(-_offset);
     if (updateKeyboardPosition)
-        _keyboard->setY(( _shown ? keyboardTop : _contentItem->height() ) + _offset );
-    _keyboard->setVisible( true );
+        _keyboard->setY((_shown ? keyboardTop : _contentItem->height()) + _offset);
+    _keyboard->setVisible(true);
 }
 
 void InjectedKeyboardPositioner::onHeightChanged()
@@ -179,12 +180,12 @@ void InjectedKeyboardPositioner::onHeightChanged()
         return;
 
     if (_shown)
-        updateContentItemPosition( true );
+        updateContentItemPosition(true);
     else
-        _keyboard->setY( _contentItem->height() + _offset ); // mantém fora da tela quando oculto
+        _keyboard->setY(_contentItem->height() + _offset); // mantém fora da tela quando oculto
 }
 
-void InjectedKeyboardPositioner::onApplicationStateChanged( Qt::ApplicationState s )
+void InjectedKeyboardPositioner::onApplicationStateChanged(Qt::ApplicationState s)
 {
     static bool wasAlreadyActive = false;
 
@@ -201,8 +202,8 @@ void InjectedKeyboardPositioner::onAnimationFinished()
     _appStateReactivated = false;
 
     if (!_shown) {
-        _contentItem->setY( 0 );
-        _keyboard->setY( _contentItem->height() + _offset ); // garante fora da tela ao finalizar hide()
+        _contentItem->setY(0);
+        _keyboard->setY(_contentItem->height() + _offset); // garante fora da tela ao finalizar hide()
     }
 }
 
@@ -211,5 +212,22 @@ qreal InjectedKeyboardPositioner::visibleKeyboardTopY() const
     if (!_contentItem || !_keyboard)
         return 0;
     // Centro + deslocamento (positivo move para baixo)
-    return (_contentItem->height() - _keyboard->height()) / 2.0 + _centerBias;
+    return (_contentItem->height() - _keyboard->height()) / 2.0 + calculateCenterBias();
+}
+
+qreal InjectedKeyboardPositioner::calculateCenterBias() const
+{
+    if (!_keyboard) {
+        return 0.0;
+    }
+
+    QScreen* screen = QGuiApplication::primaryScreen();
+    if (!screen) {
+        return 45.0;
+    }
+    qreal pixelDensity = screen->physicalDotsPerInch() / 25.4; // mm to inches conversion
+
+    qreal dpiScale = _keyboard->property("dpiScale").toReal();
+
+    return pixelDensity * 11 * (dpiScale / 100.0);
 }
