1a0b2ff3a7dcf382e8508412235b2dfa8750ee9d
Augusto Grabe <augustograbe@gmail.com>
2025-10-21 09:55:29 -0300

    fix: warnings

--- OpenVirtualKeyboard/keypressinterceptor.cpp
+++ OpenVirtualKeyboard/keypressinterceptor.cpp
@@ -4,17 +4,17 @@
  *  See accompanying LICENSE file
  */
 
-#include <QGuiApplication>
-#include <QStyleHints>
-#include <QCursor>
 #include "keypressinterceptor.h"
 #include "key.h"
+#include <QCursor>
+#include <QGuiApplication>
+#include <QStyleHints>
 
 KeyPressInterceptor::KeyPressInterceptor()
 {
-    setAcceptedMouseButtons( Qt::LeftButton );
-    setAcceptTouchEvents( true );
-    setCursor( Qt::ArrowCursor ); // to prevent Beam cursor, when e.g. InputBox is under keyboard
+    setAcceptedMouseButtons(Qt::LeftButton);
+    setAcceptTouchEvents(true);
+    setCursor(Qt::ArrowCursor); // to prevent Beam cursor, when e.g. InputBox is under keyboard
 }
 
 QQuickItem* KeyPressInterceptor::forwardTo() const
@@ -32,7 +32,7 @@ int KeyPressInterceptor::repeatInterval() const
     return _repeatInterval;
 }
 
-void KeyPressInterceptor::setForwardTo( QQuickItem* forwardTo )
+void KeyPressInterceptor::setForwardTo(QQuickItem* forwardTo)
 {
     if (_forwardTo == forwardTo)
         return;
@@ -41,7 +41,7 @@ void KeyPressInterceptor::setForwardTo( QQuickItem* forwardTo )
     emit forwardToChanged();
 }
 
-void KeyPressInterceptor::setRepeatDelay( int repeatDelay )
+void KeyPressInterceptor::setRepeatDelay(int repeatDelay)
 {
     if (_actionDelay == repeatDelay)
         return;
@@ -50,7 +50,7 @@ void KeyPressInterceptor::setRepeatDelay( int repeatDelay )
     emit repeatDelayChanged();
 }
 
-void KeyPressInterceptor::setRepeatInterval( int repeatInterval )
+void KeyPressInterceptor::setRepeatInterval(int repeatInterval)
 {
     if (_repeatInterval == repeatInterval)
         return;
@@ -59,17 +59,17 @@ void KeyPressInterceptor::setRepeatInterval( int repeatInterval )
     emit repeatIntervalChanged();
 }
 
-void KeyPressInterceptor::forwardPress( const QPointF& point )
+void KeyPressInterceptor::forwardPress(const QPointF& point)
 {
     if (!_forwardTo || _touchPointId != -1)
         return;
     _lastX = point.x();
-    auto to = findKey( point );
+    auto to = findKey(point);
 
     if (to) {
         _lastActive = to;
         if (to->isEnabled()) {
-            emit keyActivated( to );
+            emit keyActivated(to);
             to->onPressed();
             startProperTimer();
         } else {
@@ -79,7 +79,7 @@ void KeyPressInterceptor::forwardPress( const QPointF& point )
     }
 }
 
-void KeyPressInterceptor::forwardMove( const QPointF& point )
+void KeyPressInterceptor::forwardMove(const QPointF& point)
 {
     if (!_forwardTo)
         return;
@@ -87,11 +87,11 @@ void KeyPressInterceptor::forwardMove( const QPointF& point )
     _lastX = point.x();
 
     if (_alternativesOn) {
-        emit alternativePositionMoved( point.x() );
+        emit alternativePositionMoved(point.x());
         return;
     }
 
-    auto to = findKey( point );
+    auto to = findKey(point);
 
     if (!to) {
         stopTimers();
@@ -109,7 +109,7 @@ void KeyPressInterceptor::forwardMove( const QPointF& point )
             _lastActive->onExited();
         if (to->isEnabled()) {
             _lastActive = to;
-            emit keyActivated( to );
+            emit keyActivated(to);
             to->onEntered();
             startProperTimer();
         } else {
@@ -119,7 +119,7 @@ void KeyPressInterceptor::forwardMove( const QPointF& point )
     }
 }
 
-void KeyPressInterceptor::forwardRelease( const QPointF& point )
+void KeyPressInterceptor::forwardRelease(const QPointF& point)
 {
     _touchPointId = -1;
     stopTimers();
@@ -127,7 +127,7 @@ void KeyPressInterceptor::forwardRelease( const QPointF& point )
     if (_alternativesOn) {
         _alternativesOn = false;
         if (_lastActive) {
-            _lastActive->onReleased( false );
+            _lastActive->onReleased(false);
             emit alternativeSelected();
         }
         return;
@@ -136,38 +136,38 @@ void KeyPressInterceptor::forwardRelease( const QPointF& point )
     if (!_forwardTo)
         return;
 
-    auto to = findKey( point );
+    auto to = findKey(point);
 
     if (_ignoreShiftRelease && to && to->type() == Key::Shift) {
         _ignoreShiftRelease = false;
-        to->onReleased( false );
+        to->onReleased(false);
         return;
     }
 
     _ignoreShiftRelease = false;
 
     if (to && to->isEnabled()) {
-        to->onReleased( true );
-        emit keyClicked( to );
+        to->onReleased(true);
+        emit keyClicked(to);
     }
 }
 
-Key* KeyPressInterceptor::findKey( const QPointF& point ) const
+Key* KeyPressInterceptor::findKey(const QPointF& point) const
 {
     auto pos = point;
-    auto to = _forwardTo->childAt( pos.x(), pos.y() );
-    while (to && !qobject_cast<Key*>( to )) {
-        pos = _forwardTo->mapToItem( to, pos );
-        to = to->childAt( pos.x(), pos.y() );
+    auto to = _forwardTo->childAt(pos.x(), pos.y());
+    while (to && !qobject_cast<Key*>(to)) {
+        pos = _forwardTo->mapToItem(to, pos);
+        to = to->childAt(pos.x(), pos.y());
     }
-    return qobject_cast<Key*>( to );
+    return qobject_cast<Key*>(to);
 }
 
-bool KeyPressInterceptor::isTouchAllowed( const QTouchEvent::TouchPoint& point )
+bool KeyPressInterceptor::isTouchAllowed(const QTouchEvent::TouchPoint& point)
 {
     if (point.id() == _touchPointId) {
         return true;
-    } else if (_touchPointId == -1 && point.state() == Qt::TouchPointPressed) {
+    } else if (_touchPointId == -1 && point.state() == QEventPoint::State::Pressed) {
         _touchPointId = point.id();
         return true;
     }
@@ -185,85 +185,85 @@ void KeyPressInterceptor::startProperTimer()
     auto type = _lastActive->type();
 
     if (type == Key::Shift) {
-        _pressAndHoldTimer = startTimer( QGuiApplication::styleHints()->mousePressAndHoldInterval() );
+        _pressAndHoldTimer = startTimer(QGuiApplication::styleHints()->mousePressAndHoldInterval());
         return;
     }
 
     if (type == Key::Space || type == Key::Backspace) {
-        _delayTimer = startTimer( _actionDelay );
+        _delayTimer = startTimer(_actionDelay);
         return;
     }
 
     if (type == Key::KeyDefault) {
         const auto alternatives = _lastActive->alternatives();
         if (!alternatives.isValid() || alternatives.isNull())
-            _delayTimer = startTimer( _actionDelay );
+            _delayTimer = startTimer(_actionDelay);
         else
-            _alternativesTimer = startTimer( _actionDelay );
+            _alternativesTimer = startTimer(_actionDelay);
     }
 }
 
 void KeyPressInterceptor::stopTimers()
 {
-    stopTimer( _delayTimer );
-    stopTimer( _repeatTimer );
-    stopTimer( _alternativesTimer );
-    stopTimer( _pressAndHoldTimer );
+    stopTimer(_delayTimer);
+    stopTimer(_repeatTimer);
+    stopTimer(_alternativesTimer);
+    stopTimer(_pressAndHoldTimer);
 }
 
-void KeyPressInterceptor::stopTimer( int& timerId )
+void KeyPressInterceptor::stopTimer(int& timerId)
 {
     if (timerId > 0) {
-        killTimer( timerId );
+        killTimer(timerId);
         timerId = 0;
     }
 }
 
-void KeyPressInterceptor::mousePressEvent( QMouseEvent* event )
+void KeyPressInterceptor::mousePressEvent(QMouseEvent* event)
 {
-    forwardPress( event->localPos() );
+    forwardPress(event->position());
 }
 
-void KeyPressInterceptor::mouseMoveEvent( QMouseEvent* event )
+void KeyPressInterceptor::mouseMoveEvent(QMouseEvent* event)
 {
-    forwardMove( event->localPos() );
+    forwardMove(event->position());
 }
 
-void KeyPressInterceptor::mouseReleaseEvent( QMouseEvent* event )
+void KeyPressInterceptor::mouseReleaseEvent(QMouseEvent* event)
 {
-    forwardRelease( event->localPos() );
+    forwardRelease(event->position());
 }
 
-void KeyPressInterceptor::touchEvent( QTouchEvent* event )
+void KeyPressInterceptor::touchEvent(QTouchEvent* event)
 {
     switch (event->type()) {
-        case QEvent::TouchBegin:
-        case QEvent::TouchUpdate:
-        case QEvent::TouchEnd:
-            for (auto&& point : event->touchPoints()) {
-                if (!isTouchAllowed( point ))
-                    continue;
-                switch (point.state()) {
-                    case Qt::TouchPointPressed:
-                        forwardPress( point.pos() );
-                        break;
-                    case Qt::TouchPointMoved:
-                        forwardMove( point.pos() );
-                        break;
-                    case Qt::TouchPointReleased:
-                        forwardRelease( point.pos() );
-                        break;
-                    default:
-                        break;
-                }
+    case QEvent::TouchBegin:
+    case QEvent::TouchUpdate:
+    case QEvent::TouchEnd:
+        for (auto&& point : event->points()) {
+            if (!isTouchAllowed(point))
+                continue;
+            switch (point.state()) {
+            case QEventPoint::State::Pressed:
+                forwardPress(point.position());
+                break;
+            case QEventPoint::State::Updated:
+                forwardMove(point.position());
+                break;
+            case QEventPoint::State::Released:
+                forwardRelease(point.position());
+                break;
+            default:
+                break;
             }
-            break;
-        default:
-            break;
+        }
+        break;
+    default:
+        break;
     }
 }
 
-void KeyPressInterceptor::timerEvent( QTimerEvent* event )
+void KeyPressInterceptor::timerEvent(QTimerEvent* event)
 {
     if (event->timerId() == _pressAndHoldTimer) {
         stopTimers();
@@ -271,14 +271,14 @@ void KeyPressInterceptor::timerEvent( QTimerEvent* event )
         emit shiftLocked();
     } else if (event->timerId() == _delayTimer) {
         stopTimers();
-        _repeatTimer = startTimer( _repeatInterval );
+        _repeatTimer = startTimer(_repeatInterval);
     } else if (event->timerId() == _repeatTimer)
-        emit keyRepeatClicked( _lastActive );
+        emit keyRepeatClicked(_lastActive);
     else if (event->timerId() == _alternativesTimer) {
         stopTimers();
         _alternativesOn = true;
         if (_lastActive)
-            _lastActive->onReleased( false );
-        emit alternativesRequired( _lastActive, _lastX );
+            _lastActive->onReleased(false);
+        emit alternativesRequired(_lastActive, _lastX);
     }
 }
